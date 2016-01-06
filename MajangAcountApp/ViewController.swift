20da91f6ec0f05bec3d4b5dec109542e75327cc0
jjhlzn <jinjunhang@hotmail.com>
2016-01-06 20:50:57 +0800

    Initial Commit

---
+++ MajangAcountApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MajangAcountApp
+//
+//  Created by 刘兆娜 on 16/1/6.
+//  Copyright © 2016年 金军航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
